74df2ea4920bb63f3fb3a52d3844da43baf0465a
Samuel Gélineau <samuel.gelineau@keatext.com>
2016-02-23 13:45:10 -0500

    StringBasedEnumeration, with json serialization

---
+++ src/main/scala/com/keatext/mintosci/scalautils/StringBasedEnumeration.scala
@@ -0,0 +1,20 @@
+package com.keatext.mintosci.scalautils
+
+import spray.json._
+
+
+trait StringBasedEnumeration extends Enumeration {
+  def valueOf(name: String): Option[Value] = values.find(_.toString == name)
+
+  val jsonFormat = new RootJsonFormat[Value] {
+    def write(value: Value) = JsString(value.toString)
+
+    def read(value: JsValue): Value = value match {
+      case JsString(s) => valueOf(s) match {
+        case Some(x) => x
+        case None => deserializationError(s"Expected one of ${values}, but got ${s}")
+      }
+      case x => deserializationError(s"Expected JsString, but got ${x}")
+    }
+  }
+}
