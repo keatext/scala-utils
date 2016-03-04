c48e495ace2591aa5fa59755ac479cf047e3a5d9
Marc-Olivier Ricard <moricard@keatext.com>
2016-03-04 13:42:35 -0500

    Generalize unzip to GenTraversableLike

--- src/main/scala/com/keatext/mintosci/scalautils/Unzip4.scala
+++ src/main/scala/com/keatext/mintosci/scalautils/Unzip4.scala
@@ -1,8 +1,19 @@
 package com.keatext.mintosci.scalautils
 
+import scala.collection.GenTraversableLike
+import scala.collection.generic.CanBuildFrom
+
+
 object Unzip4 {
-  def unapply[A, B, C, D](tuples: List[(A, B, C, D)])
-  : Some[(List[A], List[B], List[C], List[D])] =
+  def unapply[A, B, C, D, Repr, AA, BB, CC, DD](
+    tuples: GenTraversableLike[(A, B, C, D), Repr]
+  )(implicit
+    cbfA: CanBuildFrom[Repr, A, AA],
+    cbfB: CanBuildFrom[Repr, B, BB],
+    cbfC: CanBuildFrom[Repr, C, CC],
+    cbfD: CanBuildFrom[Repr, D, DD]
+  )
+  : Some[(AA, BB, CC, DD)] =
     Some((
       tuples.map(_._1),
       tuples.map(_._2),
