ddbc88eccab2794ccf01d65cad04e88662d1de0d
Samuel Gélineau <samuel.gelineau@keatext.com>
2016-03-10 17:57:14 -0500

    only traverse the list once
    we were building a temporary queue in order to avoid using the builder's
    mutable operations, but in the end we have to use its mutable operations
    anyway, so we weren't really gaining anything.

--- src/main/scala/com/keatext/mintosci/scalautils/FutureTraverse.scala
+++ src/main/scala/com/keatext/mintosci/scalautils/FutureTraverse.scala
@@ -2,14 +2,11 @@ package com.keatext.mintosci.scalautils
 
 import scala.collection.GenTraversableLike
 import scala.collection.generic.CanBuildFrom
-import scala.collection.immutable.{Queue, Stack}
 import scala.collection.mutable.Builder
 import scala.concurrent.{ExecutionContext, Future}
 
-/**
-  * Implements a way to execute Future operations sequentially and wrap them in a global Future
-  * (note that Future.sequence is __not__ sequential so we need something better)
-  */
+// Future.traverse runs the futures in parallel,
+// these methods run them one after the other.
 object FutureTraverse {
   def traverse[A, B, Repr, That](
     objects: GenTraversableLike[A,Repr]
@@ -19,19 +16,16 @@ object FutureTraverse {
     implicit cbf: CanBuildFrom[Repr, B, That],
     ec: ExecutionContext
   )
-  : Future[That] = {
-    objects.foldLeft[Future[Queue[B]]](
-      Future.successful(Queue())
-    ) { (futureElems, a) =>
+  : Future[That] =
+    objects.foldLeft[Future[Builder[B,That]]](
+      Future.successful(cbf())
+    ) { (futureBuilder, a) =>
       for {
-        elems <- futureElems
+        builder <- futureBuilder
         elem <- f(a)
-      } yield elems :+ elem
-    }.map { queue =>
-      val builder: Builder[B,That] = cbf()
-      builder ++= queue
+      } yield builder += elem
+    }.map { builder =>
       builder.result()
     }
-  }
 }
 
